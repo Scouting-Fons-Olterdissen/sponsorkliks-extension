b6380f5aabcd8fcf5162204641622a83d978b94b
Floris Jansen <florismartijnjansen@gmail.com>
2022-11-06 16:04:17 +0100

    make options work with arrow functions

--- options.js
+++ options.js
@@ -3,33 +3,23 @@
 // Saves options to browser.storage.sync.
 function save_options() {
 	var autoForward = document.getElementById("autoForward").checked;
-	browser.storage.sync.set(
-		{
-			autoForward: autoForward,
-		},
-		function () {
-			// Update status to let user know options were saved.
-			var status = document.getElementById("status");
-			status.textContent = "Wijzigingen opgeslagen.";
-			setTimeout(function () {
-				status.textContent = "";
-			}, 750);
-		}
-	);
+	browser.storage.local.set({ autoForward }, () => {
+		// Update status to let user know options were saved.
+		var status = document.getElementById("status");
+		status.textContent = "Wijzigingen opgeslagen.";
+		setTimeout(function () {
+			status.textContent = "";
+		}, 750);
+	});
 }
 
 // Restores select box and checkbox state using the preferences
 // stored in browser.storage.
 function restore_options() {
 	// Use default value autoForward = false.
-	browser.storage.sync.get(
-		{
-			autoForward: false,
-		},
-		function (items) {
-			document.getElementById("autoForward").checked = items.autoForward;
-		}
-	);
+	browser.storage.local.get({ autoForward: false }, (items) => {
+		document.getElementById("autoForward").checked = items.autoForward;
+	});
 }
 document.addEventListener("DOMContentLoaded", restore_options);
 document.getElementById("save").addEventListener("click", save_options);
